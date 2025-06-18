426e07d16c64b363e9c596152f5b06f7ca3a06ae
chack <mrstas1330@mail.ru>
2025-06-18 15:31:29 +0300

    fix: fix mail

--- server/src/utils/mail.js
+++ server/src/utils/mail.js
@@ -1,9 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  // secure: true,
-  // port: 465,
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
